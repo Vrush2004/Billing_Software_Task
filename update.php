d98f4196a95179c9863b0c8725dd15c088cb4f0b
Vrush2004 <vrushalichaube@gmail.com>
2024-09-27 13:29:52 +0530

    update

--- update.php
+++ update.php
@@ -25,3 +25,44 @@
         }
     }
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>CURD Operation</title>
+  </head>
+  <body>
+    <div class="container my-5">
+    <form method="post">
+  <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" value=<?php echo $name;?>>
+  </div>
+  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email" value=<?php echo $email;?>>
+  </div>
+  <div class="form-group">
+    <label>Mobile Number</label>
+    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" value=<?php echo $mobile;?>>
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" placeholder="Enter your password" name="password" value=<?php echo $password;?>>
+  </div>
+  
+  <button type="submit" class="btn btn-primary" name="submit">Update</button>
+</form>
+
+    </div>
+
+    
+  </body>
+</html>
